74e9be5719301fa177762a7b12f91783d051fa43
coneill000 <coneill000@gmail.com>
2020-04-30 13:07:42 -0400

    Add initilization of main app

--- www/js/newAccount.js
+++ www/js/newAccount.js
@@ -123,5 +123,37 @@ function checkLogin() {
     if(!correct_login) {
         h2.appendChild(document.createTextNode("Username or password is incorrect."));
         workspace.append(h2);
+    } else {
+        initializeApp();
     }
+}
+
+function initializeApp() {
+    let workspace = document.getElementById("content");
+    workspace.innerHTML = "";
+    
+    let header = document.createElement("div");
+    header.classList.add("navbar");
+    header.setAttribute("id", "header");
+    
+    let searchButton = document.createElement("button");
+    searchButton.setAttribute("id", "search");
+    searchButton.classList.add("tablinks");
+    searchButton.appendChild(document.createTextNode("Search"));
+                            
+    let favoriteButton = document.createElement("button");
+    favoriteButton.setAttribute("id", "favorite");
+    favoriteButton.classList.add("tablinks");
+    favoriteButton.appendChild(document.createTextNode("Favorites")); 
+                               
+    let profileButton = document.createElement("button");
+    profileButton.setAttribute("id", "profile");
+    profileButton.classList.add("tablinks");
+    profileButton.appendChild(document.createTextNode("Profile"));
+                              
+    header.appendChild(searchButton);
+    header.appendChild(favoriteButton);
+    header.appendChild(profileButton);
+    
+    workspace.append(header);
 }
