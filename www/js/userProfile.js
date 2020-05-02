f35ccf3aae5c4a30bfc68b06b0bebbf816721c3f
coneill000 <coneill000@gmail.com>
2020-05-02 17:54:16 -0400

    Add delete account functionality

--- www/js/userProfile.js
+++ www/js/userProfile.js
@@ -24,4 +24,40 @@ function loadProfile(userId) {
         dispLogin("Logout successful");
     };
     workspace.append(logoutButton);
+    
+    let deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Delete Account"));
+    deleteButton.onclick = function () {
+        deleteAccount(userId);
+    };
+    workspace.append(deleteButton);
+}
+
+function deleteAccount(userId) {
+    console.log("Entering deleteAccount function");
+    let users = JSON.parse(window.localStorage.getItem("users"));
+    let workspace = document.getElementById("content");
+    workspace.innerHTML = "";
+    let confirmMessage = document.createElement("p");
+    confirmMessage.appendChild(document.createTextNode("Are you sure you want to delete your account?"));
+    let confirmButton = document.createElement("button");
+    confirmButton.appendChild(document.createTextNode("Yes, delete my account."));
+    confirmButton.onclick = function () {
+        console.log(`Deleting account at index ${userId}`);
+        users.splice(userId, 1);
+        window.localStorage.setItem("users", JSON.stringify(users));
+        let thing = window.localStorage.getItem("users");
+        console.log(JSON.parse(thing));
+        dispLogin("Account deleted");
+    };
+    let backButton = document.createElement("button");
+    backButton.appendChild(document.createTextNode("No, I've decided to keep my account"));
+    backButton.onclick = function () {
+        loadProfile(userId);
+    };
+    
+    workspace.append(confirmMessage);
+    workspace.append(confirmButton);
+    workspace.append(backButton);
+    
 }
